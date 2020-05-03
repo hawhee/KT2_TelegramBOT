2a979a22604c941ff63d96d311571e51d5fc75be
hawhee <58119126+hawhee@users.noreply.github.com>
2020-05-03 23:52:30 +0300

    init

---
+++ bot.py
@@ -0,0 +1,25 @@
+import telebot
+import pypwned
+
+from config import TOKEN
+from config import hibp_key
+
+
+bot = telebot.TeleBot(token=TOKEN)
+
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id,"Введите почту для проверки")
+
+
+@bot.message_handler(content_types=['text'])
+def echo(message):
+    email = message.text
+    result = str(pwned(hibp_key).getAllBreachesForAccount(str(email))))
+    bot.send_message(message.chat.id, result)
+
+
+
+
+bot.polling()
---
+++ config.py
@@ -0,0 +1,2 @@
+TOKEN ="<Your TOKEN>"
+hibp_key = "<key,Если у вас его нет, то можете купить на https://haveibeenpwned.com/API/Key >"
